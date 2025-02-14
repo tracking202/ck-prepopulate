88cc7e2cd7fa9821101b1eaf5f61f97c7aedebcf
Nana GB <tracking202@users.noreply.github.com>
2025-02-13 22:39:52 -0800

    Update pre-pop.js

--- pre-pop.js
+++ pre-pop.js
@@ -1,17 +1,24 @@
-getCKUrlParam = (urlParam) => {
-	const queryString = window.location.search;
-	const urlParams = new URLSearchParams(queryString);
-	const lcParams = new URLSearchParams();
+const getCKUrlParam = (urlParam) => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
 
-	for (const [name, value] of urlParams) {
-		lcParams.append(name.toLowerCase(), value);
-	}
-	
-	return lcParams.get(urlParam.toLowerCase());
+    for (const [name, value] of urlParams) {
+        if (name.toLowerCase() === urlParam.toLowerCase()) {
+            return value;
+        }
+    }
+    return null;
 };
 
 const name = getCKUrlParam("fn");
 const email = getCKUrlParam("em");
 
-document.getElementsByName("fields[first_name]")[0].value = name;
-document.getElementsByName("email_address")[0].value = email;
+const firstNameField = document.getElementsByName("fields[first_name]")[0];
+const emailField = document.getElementsByName("email_address")[0];
+
+if (firstNameField) {
+    firstNameField.value = name || '';
+}
+if (emailField) {
+    emailField.value = email || '';
+}
